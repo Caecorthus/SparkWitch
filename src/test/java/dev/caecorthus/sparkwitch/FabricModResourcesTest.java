364032d394731e5d0b7cf8cdde1eb8e82deaadad
Kricy <124311311+Kricium@users.noreply.github.com>
2026-06-28 22:34:24 -0400

    Allow servers to omit LambDynamicLights

--- src/test/java/dev/caecorthus/sparkwitch/FabricModResourcesTest.java
+++ src/test/java/dev/caecorthus/sparkwitch/FabricModResourcesTest.java
@@ -9,20 +9,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class FabricModResourcesTest {
     private static final Path FABRIC_MOD_JSON = Path.of("src/main/resources/fabric.mod.json");
     private static final String FLASHLIGHT_INITIALIZER = "dev.caecorthus.sparkwitch.client.FlashlightDynamicLightsInitializer";
 
     @Test
-    void declaresLambDynamicLightsForFlashlightLineLight() throws IOException {
+    void declaresLambDynamicLightsEntrypointWithoutServerDependency() throws IOException {
         JsonObject mod = JsonParser.parseString(Files.readString(FABRIC_MOD_JSON)).getAsJsonObject();
         JsonObject entrypoints = mod.getAsJsonObject("entrypoints");
         JsonObject depends = mod.getAsJsonObject("depends");
 
         assertEquals(FLASHLIGHT_INITIALIZER, entrypoints.getAsJsonArray("lambdynlights:initializer").get(0).getAsString());
         assertEquals(FLASHLIGHT_INITIALIZER, entrypoints.getAsJsonArray("dynamiclights").get(0).getAsString());
-        assertTrue(depends.get("lambdynlights").getAsString().contains("4.8.7+1.21.1"));
+        assertFalse(depends.has("lambdynlights"));
     }
 }
